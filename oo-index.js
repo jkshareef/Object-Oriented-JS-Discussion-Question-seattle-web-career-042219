f9383797092b260ee7f0476684c7e6a821a80328
jkshareef <jamalkshareef@gmail.com>
2019-06-11 11:29:50 -0700

    Done.

--- oo-index.js
+++ oo-index.js
@@ -1,3 +1,5 @@
+
+
 class Unicorn {
   constructor(name, occupation) {
     this.name = name;
@@ -5,4 +7,62 @@ class Unicorn {
     this.health = 1000
   }
 
+  restart() {
+    while (eventArea.hasChildNodes()) {
+        eventArea.removeChild(eventArea.lastChild);
+    }
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("Out on the street, ready to go!"));
+    eventArea.prepend(div);
+    imageArea.className = 'new';
+  }
+
+  attack() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("'ATTAAAAAAAAAK!''"));
+    eventArea.prepend(div);
+    imageArea.className = 'attack';
+  }
+  
+  defend() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("You put sunglasses on."));
+    eventArea.prepend(div);
+    imageArea.className = 'defend';
+  }
+  
+  heal() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("You feel refreshed after yoga."));
+    eventArea.prepend(div);
+    imageArea.className = 'heal';
+  }
+  
+  breakdance() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("It's really spinning class"));
+    eventArea.prepend(div);
+    imageArea.className = 'breakdance';
+  }
+  
+  eatCronut() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("You've gained 1000 health points."));
+    eventArea.prepend(div);
+    imageArea.className = 'eat';
+  }
+
 }
+
+let janice = new Unicorn('Janice', 'Consulting')
+
+const eventArea = document.getElementById('event_area')
+const imageArea = document.getElementById('image_area')
+const restartButton = document.getElementById('restart').addEventListener("click",()=> janice.restart())
+const attackButton = document.getElementById('attack').addEventListener("click",()=> janice.attack())
+const defendButton = document.getElementById('defend').addEventListener("click",()=> janice.defend())
+const healButton = document.getElementById('heal').addEventListener("click",()=> janice.heal())
+const breakdanceButton = document.getElementById('breakdance').addEventListener("click",()=> janice.breakdance())
+const eatButton = document.getElementById('eat').addEventListener("click",()=> janice.eatCronut())
+
+
